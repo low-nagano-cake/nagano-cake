f6e2fdb532bb0dd020de611aabdb42f8882f21f5
symnbe <ookaminingen@me.com>
2023-03-21 05:39:43 +0000

    [Addf]admin/views/layouts/header&footer_html

--- config/routes.rb
+++ config/routes.rb
@@ -2,31 +2,32 @@ Rails.application.routes.draw do
 
 ## Customer
 # ルート
-root :to => "public/homes#top"
+  root :to => "public/homes#top"
 
 
-scope :public do
-  resources :homes, only: [:top,:about]
-end
+  scope :public do
+    resources :homes, only: [:top,:about]
+  end
 
 
-# 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
 
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 
-namespace :admin do
-resources :items, only: [:index, :show, :new, :create, :show]
-resources :genres, only: [:index, :create, :edit, :update]
+  namespace :admin do
+  resources :items, only: [:index, :show, :new, :create, :show]
+  resources :genres, only: [:index, :create, :edit, :update]
+  get 'homes/top' => 'admin/homes/top', as: '/top'
 end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
