875290a597ab71029a4155cb0284e2ebb1e9a69c
rumi-000 <rumi.sato9797@gmail.com>
2023-03-22 05:33:05 +0000

    adminログイン確認要2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,14 @@ def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(Admin)
         admin_path
     else
-      # # カスタマー側のトップページ(rumi)
+      #カスタマー側のトップページ(rumi)
         root_path
     end
 end
 
 def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :user
-        # カスタマー側のトップページ(rumi)
+        #カスタマー側のトップページ(rumi)
         root_path
     elsif resource_or_scope == :admin
         new_admin_session_path
