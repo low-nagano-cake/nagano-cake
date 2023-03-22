96d2542e7af066b2f57fc5c9f056087cd142f700
symnbe <ookaminingen@me.com>
2023-03-22 15:44:06 +0000

    [add]Gem_enum

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,6 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         enum status_method: { 有効: 0, 退会: 1 }
 end
