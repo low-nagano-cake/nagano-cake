45d27fa505c8821649f1c23c457e9d7fe2c63ad9
symnbe <ookaminingen@me.com>
2023-03-23 09:21:00 +0000

    [Add]admin/order_detail/show_html

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+
+    enum status: { wait: 0, payment_confirmation: 1, progression: 2, prepare: 3, sent: 4 }
+
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,7 @@
 class OrderDetail < ApplicationRecord
+  
+  
+      enum making_status: { impossible: 0, wait: 1, progression: 2, completion: 3 }
+
+  
 end
