53f69dc3f5a73f7e941bfcf3b2ef611663598287
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-03-23 07:35:48 +0000

    [Fix] 配送先削除後のリダイレクト先修正

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -34,7 +34,7 @@ class Public::AddressesController < ApplicationController
   def destroy
     @address = Address.find(params[:id])
     @address.destroy
-    redirect_to addresses_path(current_customer.id)
+    redirect_to addresses_path
   end
   
   
