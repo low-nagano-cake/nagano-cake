83d5aff504c1b758e230338ad986a5a54b31edc5
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-03-24 13:11:39 +0000

    [Add] order_detailsとのアソシエーションとenumの表記記述

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,11 @@
 class Order < ApplicationRecord
   
+  enum status: { wait: 0, payment_confirmation: 1, progression: 2, prepare: 3, sent: 4 }
   
   belongs_to :customer
+  
+  has_many :order_details
+  
+  
   
 end
