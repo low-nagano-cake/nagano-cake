a6a1ae21c0539948d727c9efe0063b38cda7b07a
symnbe <ookaminingen@me.com>
2023-03-25 10:00:18 +0000

    [fix]注文履歴一覧

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
 has_one_attached :image
 belongs_to :genre
 
-has_many :cart_item, dependent: :destroy
+has_many :cart_items, dependent: :destroy
 has_many :order_details, dependent: :destroy
 
  def add_tax_price
