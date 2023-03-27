5a0abdd0a9199acdbd55dae83ff256aa5fb03650
rumi-000 <rumi.sato9797@gmail.com>
2023-03-27 00:44:30 +0000

    権限の追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,8 @@ belongs_to :genre
 has_many :cart_items, dependent: :destroy
 has_many :order_details, dependent: :destroy
 
+validates :image, presence: true
+
  def add_tax_price
    (self.price * 1.10).round
  end
